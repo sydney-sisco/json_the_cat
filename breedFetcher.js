bfdacfd20a9b56d58f2001e17027215493f42643
Syd Sisco <hello@sydsis.co>
2021-02-10 23:05:21 +0000

    lints

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,13 +2,13 @@ const request = require('request');
 
 const breed = process.argv[2];
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {  
-  if (error) { 
+request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
+  if (error) {
     console.log(error);
   } else {
     const data = JSON.parse(body);
     if (data.length === 0) {
-      console.log(`Sorry, breed ${breed} was not found.`)
+      console.log(`Sorry, breed ${breed} was not found.`);
     } else {
       console.log(data[0].description);
     }
